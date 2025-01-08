d743222850858927d9138951106f10fae92639ee
harkamal <gajinder@zeam.in>
2025-01-08 18:06:22 +0530

    add and start with gosssiub behav

--- rust-libp2p/src/lib.rs
+++ rust-libp2p/src/lib.rs
@@ -1,12 +1,13 @@
 use libp2p::core::{multiaddr::Protocol, multiaddr::Multiaddr, muxing::StreamMuxerBox, transport::Boxed};
 use libp2p::identity::{secp256k1, Keypair};
-use libp2p::{identify, identity, core, noise, ping, yamux, PeerId, Transport, SwarmBuilder};
+use libp2p::{gossipsub, identify, identity, core, noise, ping, yamux, PeerId, Transport, SwarmBuilder};
 use libp2p::swarm::{NetworkBehaviour, SwarmEvent};
 use std::time::Duration;
 use futures::future::Either;
-use std::{error::Error, net::Ipv4Addr};
+use std::{error::Error, net::Ipv4Addr,collections::hash_map::DefaultHasher,hash::{Hash, Hasher},};
 use futures::StreamExt;
 use slog::{crit, debug, info, o, trace, warn};
+use tokio::{io, io::AsyncBufReadExt, select};
 
 type BoxedTransport = Boxed<(PeerId, StreamMuxerBox)>;
 
@@ -25,16 +26,41 @@ extern "C" {
 struct Behaviour {
     identify: identify::Behaviour,
     ping: ping::Behaviour,
+    gossipsub: gossipsub::Behaviour,
 }
 
 impl Behaviour {
-    fn new(local_public_key: identity::PublicKey) -> Self {
+    fn new(key: identity::Keypair) -> Self {
+        let local_public_key = key.public();
+        // To content-address message, we can take the hash of message and use it as an ID.
+        let message_id_fn = |message: &gossipsub::Message| {
+            let mut s = DefaultHasher::new();
+            message.data.hash(&mut s);
+            gossipsub::MessageId::from(s.finish().to_string())
+        };
+
+        // Set a custom gossipsub configuration
+        let gossipsub_config = gossipsub::ConfigBuilder::default()
+        // .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
+        .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message
+        // signing)
+        .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
+        .build().unwrap();
+        // .map_err(|msg| io::Error::new(io::ErrorKind::Other, msg))?; // Temporary hack because `build` does not return a proper `std::error::Error`.
+
+        // build a gossipsub network behaviour
+        let gossipsub = gossipsub::Behaviour::new(
+            gossipsub::MessageAuthenticity::Signed(key.clone()),
+            gossipsub_config,
+        ).unwrap();
+
         Self {
             identify: identify::Behaviour::new(identify::Config::new(
                 "/ipfs/0.1.0".into(),
                 local_public_key.clone(),
             )),
             ping: ping::Behaviour::default(),
+            gossipsub,
         }
     }
 }
@@ -63,7 +89,7 @@ async fn build_network(selfPort: i32, connectPort: i32) {
         .with_other_transport(|_key| transport)
         .expect("infalible");
     let mut swarm = builder
-    .with_behaviour(|key| Behaviour::new(key.public())).unwrap()
+    .with_behaviour(|key| Behaviour::new(key.clone())).unwrap()
     .with_swarm_config(|cfg| cfg.with_idle_connection_timeout(Duration::from_secs(u64::MAX)))
     .build();
 
